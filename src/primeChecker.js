759f4fb631a586b73930de9c1c95009bdded7290
stephengeller <stephen.geller@hotmail.com>
2017-11-21 21:22:27 +0000

    prime checker working

---
+++ src/primeChecker.js
@@ -0,0 +1,10 @@
+class PrimeChecker {
+  checkPrime(number) {
+    for (let i = 2; i < number; i++) {
+      if (number % i === 0) return false;
+    }
+    return number !== 1;
+  }
+}
+
+module.exports = PrimeChecker;
--- test/primeChecker.test.js
+++ test/primeChecker.test.js
@@ -1,7 +1,21 @@
 const PrimeChecker = require('../src/primeChecker');
 
 describe('PrimeChecker', () => {
+  let primeChecker;
+  beforeEach(() => {
+    primeChecker = new PrimeChecker();
+  });
+
   it('exists', () => {
     expect(PrimeChecker).toBeDefined();
+  });
+
+  describe('#checkPrime', () => {
+    it('returns true if number is prime', () => {
+      expect(primeChecker.checkPrime(5)).toBe(true);
+    });
+    it('returns false if number is not prime', () => {
+      expect(primeChecker.checkPrime(6)).toBe(false);
+    });
   });
 });
