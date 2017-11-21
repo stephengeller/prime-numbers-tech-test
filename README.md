b979e45e966bbbc39450e3d24ebde27a1d4d619e
stephengeller <stephen.geller@hotmail.com>
2017-11-21 21:06:21 +0000

    first passing test

---
+++ README.md
@@ -0,0 +1,3 @@
+# Prime Numbers Tech test
+
+Watch this space!
---
+++ test/primeChecker.test.js
@@ -0,0 +1,7 @@
+const PrimeChecker = require('../src/primeChecker');
+
+describe('PrimeChecker', () => {
+  it('exists', () => {
+    expect(PrimeChecker).toBeDefined();
+  });
+});
